7bc50eb5d93cd9a4108fce2261e42bf7a8584941
up202208561-code <up202208561@g.uporto.pt>
2026-07-09 16:10:04 +0100

    Update excel.py

--- excel.py
+++ excel.py
@@ -288,7 +288,7 @@ def criar_projeto(ficheiro, dados_projeto):
             row=nova_linha,
             column=coluna
         ).value = formula.format(r=nova_linha)
-    ws[f"S{nova_linha}"] = f"=Q{nova_linha}+R{nova_linha}"
+
     wb.save(temp.name)
 
     return temp.name
