74b75aeb70a093205fbe74b7662f76dc41bf1a59
up202208561-code <up202208561@g.uporto.pt>
2026-07-09 14:18:37 +0100

    Update formulas.py

--- formulas.py
+++ formulas.py
@@ -1,1 +1,63 @@
+FORMULAS = {
 
+    12: "=SUM(H{r}+I{r}+J{r}+K{r})",
+
+    16: "=N{r}+O{r}",
+
+    19: "=Q{r}+R{r}",
+
+    22: "=SUM(N{r}:O{r})+SUM(Q{r}:R{r})+T{r}",
+
+    27: "=W{r}*Dados!$D$4+X{r}*Dados!$D$5+Z{r}*Dados!$D$6",
+
+    28: "=SUM(W{r}:X{r})+Z{r}",
+
+    33: "=AC{r}+AE{r}",
+
+    34: "=V{r}+AB{r}+AG{r}",
+
+    35: "=P{r}+S{r}+T{r}+W{r}+Y{r}+Z{r}+AG{r}",
+
+    37: "=ROUND(AJ{r}*N{r},0)",
+
+    38: "=ROUND(AJ{r}*O{r},0)",
+
+    39: "=AK{r}+AL{r}",
+
+    40: "=ROUND(AJ{r}*Q{r},0)",
+
+    41: "=ROUND(AJ{r}*R{r},0)",
+
+    42: "=ROUND(AJ{r}*S{r},0)",
+
+    43: "=ROUND(AJ{r}*T{r},0)",
+
+    44: "=AM{r}*Dados!$D$4+SUM(AN{r}:AO{r})*Dados!$D$5+AQ{r}*Dados!$D$6",
+
+    45: "=AM{r}+SUM(AN{r}:AO{r})+AQ{r}",
+
+    47: "=(W{r}+Y{r}+Z{r})+(AC{r}+AE{r})+(AM{r}+AP{r}+AQ{r})",
+
+    48: "=AU{r}*Dados!$F$25",
+
+    50: "=AS{r}+AB{r}+AG{r}",
+
+    51: "=M{r}+AT{r}+AV{r}+AW{r}",
+
+    53: "=AY{r}-AZ{r}",
+
+    54: "=AR{r}+AA{r}+U{r}+AD{r}",
+
+    55: "=IF(AX{r}>0,AH{r}/(L{r}/1000),0)",
+
+    56: "=IF(BC{r}>0,BB{r}/AX{r},0)",
+
+    57: "=IF(BC{r}>0,(BB{r}*0.011)/AX{r},0)",
+
+    58: "=IF(BF{r}>0,BA{r}/AX{r},0)",
+
+    59: "=IF(BF{r}>0,BA{r}/BB{r},0)",
+
+    60: "=IF(BF{r}>0,BA{r}/(BB{r}*0.011),0)"
+
+}
