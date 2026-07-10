f1663b0e13eabb383a81ecfbb1f5e838ffe90cd2
up202208561-code <up202208561@g.uporto.pt>
2026-07-10 09:38:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,8 +88,22 @@ with tab_editar:
     )
 
     if resultados.empty:
+
         st.warning("Nenhum projeto encontrado.")
-        st.stop()
+
+    else:
+    
+        escolha = st.selectbox(
+            "Projeto",
+            resultados["RefObra"].astype(str)
+        )
+
+    # daqui para baixo fica TODO o código da edição
+    # abrir workbook
+    # procurar linha
+    # ler projeto
+    # formulário
+    # botão guardar
 
     # TODO:
     # daqui para baixo fica TODO o código da edição:
